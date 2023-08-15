75ccadb2df73200cecfdb4b633bdeac260819533
gal tager <gal@alterno.io>
2023-08-15 16:45:33 +0300

    TodoListPage

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 
 <template>
   <div id="app">
-    <HelloWorld />
+    <TodoListPage />
   </div>
 </template>
 
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import App from './App.vue'
 import Header from './components/Header.vue'
 import Table from './components/Table.vue'
 import Footer from './components/Footer.vue'
-import HelloWorld from './components/HelloWorld.vue'
+import TodoListPage from './components/TodoListPage.vue'
 import Todo from './components/Todo.vue'
 
 import completeSVG from './assets/completeSVG.vue'
@@ -17,7 +17,7 @@ const app = createApp(App);
 app.component('Header',Header)
 app.component('Table',Table)
 app.component('Footer',Footer)
-app.component('HelloWorld',HelloWorld)
+app.component('TodoListPage',TodoListPage)
 app.component('Todo',Todo)
 
 // svgs
